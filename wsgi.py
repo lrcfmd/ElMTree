4982dd355809e15c3f77b0e9e7530bf301aa1b1a
SurgeArrester <cameron.h@rgreaves.me.uk>
2023-06-30 14:39:44 +0100

    Removed some comments

--- wsgi.py
+++ wsgi.py
@@ -4,10 +4,8 @@ from site import getsitepackages
 wd = os.getcwd()
 sitepack =getsitepackages()
 
-#raise Exception(f"wd: {wd}\nsp: {sitepack}")
-
 from app import app
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
 
