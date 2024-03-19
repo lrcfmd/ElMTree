70f0726863010b1010fbac760bc8ce9842c38d0e
Cameron Hargreaves <cameron.h@rgreaves.me.uk>
2024-03-19 03:28:05 +0100

    optized string only indexing

--- ElMTree/ElMTree.py
+++ ElMTree/ElMTree.py
@@ -19,14 +19,16 @@ from tqdm.contrib.concurrent import process_map
 from scipy.spatial.distance import euclidean
 
 from ElMD import ElMD
+from pymatgen.core import Structure
+import pandas as pd
 
 def main():
     from matminer.datasets import load_dataset
     
     df = load_dataset("matbench_expt_gap").head(2000)
 
-    compositions = [(x, i) for i, x in enumerate(df['composition'])]
-    compositions = [x[0] for x in pk.load(open('optimade_comps.pk', 'rb'))]
+    compositions = [x for i, x in enumerate(df['composition'])]
+    # compositions = [x[0] for x in pk.load(open('data/optimade_comps.pk', 'rb'))]
 
     # for composition, code in compositions:
     #     elmd_comp = ElMD(composition)
@@ -46,14 +48,25 @@ def main():
     # if not os.path.exists('db_lookup.pk'):
     #     pk.dump(db_lookup, open('db_lookup.pk', 'wb'))
 
-    elmtree = ElMTree(compositions)
+    df_names  = ['df_outputs_filtout_v1', 'df_outputs_filtout_v6', 'df_outputs_v5', 'df_outputs_v6']
+    dfs = [pd.read_pickle('data/' + x + '.pkl') for x in df_names]
+
+    re2fractive_comps, dataset_identifier = zip(*[(str(Structure.from_dict(x).composition), i) for i, df in enumerate(dfs) for x in df['structure']])
+    optimade_comps = [x[0] for x in pk.load(open('data/optimade_comps.pk', 'rb'))][:1000]
+
+    compositions = optimade_comps + list(re2fractive_comps)
+
+    elmtree = ElMTree(compositions, verbose=True)
+
+    x = elmtree.knn(compositions[0])
+    print(x)
 
     pk.dump(elmtree, open("indexedElMTree.pk", "wb"))
 
 
 class ElMTree():
     def __init__(self, 
-                 points, # Points to be indexed, assumed ElMD objects TODO generalize?
+                 input_compositions, # input_compositions to be indexed, assumed ElMD objects TODO generalize?
                  assigned_metric=ElMD("", metric="fast").elmd, 
                  centroid_ratio=32, 
                  on_disk=False,
@@ -66,7 +79,7 @@ class ElMTree():
         
         self.assigned_metric = assigned_metric
         self.centroid_ratio = centroid_ratio
-        self.n = len(points)
+        self.n = len(input_compositions)
         self.m = int(self.n / self.centroid_ratio)
         self.on_disk = on_disk 
 
@@ -82,42 +95,50 @@ class ElMTree():
         self.max_workers = max_workers
 
         # Significantly faster, but can take 100s of GBs RAM for larger datasets
-        if pre_process is None:
-            if len(points) * 4 < 3000000:
+        if pre_process is None or pre_process:
+            if len(input_compositions) < 20000:
                 if self.verbose: print("Preprocessing formula into ElMD objects")
-                points = [ElMD(point) for point in points]
+                input_compositions = [ElMD(input_composition) for input_composition in input_compositions]
 
-        if pre_process:
-            points = [ElMD(point) for point in points]
-
-        # Select m points to be our centres at random. 
-        # Each list item stores the centre point object, a list of children, 
+        # Select m input_compositions to be our centres at random. 
+        # Each list item stores the centre input_composition object, a list of children, 
         # and the covering radius
-        self.centres = [[ElMD(point), [], 0] for point in random.sample(points, k=self.m)]
+        if self.verbose: print("Selecting Voronoi centroids")
 
+        self.centres = [[ElMD(input_composition), [], 0] for input_composition in random.sample(input_compositions, k=self.m)]
+        # self.centres = [[input_composition, [], 0] for input_composition in random.sample(input_compositions, k=self.m)]
+        
         self.lookup_tables = lookup_tables
+
+        if self.verbose: print("Updating composition index lookup")
 
         if lookup_tables:
             # Written in separate scripts for the ElMTree to check which databases, and which type of db each one is
             self.elmtree_lookup = pk.load(open(elmtree_lookup_path, "rb")) # Returns the dbs a composition string can be found in 
             self.db_lookup = pk.load(open(db_lookup_path, "rb")) # Gives the metadata about whether the db is experimental or contains structural information
         
-        elif isinstance(points[0], ElMD):
+        elif isinstance(input_compositions[0], ElMD):
             # If there are no separate db lookups, simply index each composition by its position
-            self.elmtree_lookup = {point.pretty_formula: i for i, point in enumerate(points)}
+            self.elmtree_lookup = {input_composition.pretty_formula: i for i, input_composition in enumerate(input_compositions)}
 
         else:
             if self.verbose: 
-                print("Constructing initial lookup")
-                self.elmtree_lookup = {ElMD(point).pretty_formula: i for i, point in tqdm(enumerate(points), total=len(points))}
+                self.elmtree_lookup = {ElMD(input_composition).pretty_formula: i for i, input_composition in tqdm(enumerate(input_compositions), total=len(input_compositions))}
             else:
-                self.elmtree_lookup = {ElMD(point).pretty_formula: i for i, point in enumerate(points)}
+                self.elmtree_lookup = {ElMD(input_composition).pretty_formula: i for i, input_composition in enumerate(input_compositions)}
 
-        assignments = process_map(self.get_centroid, points, chunksize=100, max_workers=self.max_workers)
+        if self.verbose: print("Assigning compositions to centroids")
 
-        for point, ind, distance in assignments:
+        assignments = process_map(self.get_centroid, input_compositions, chunksize=1000, max_workers=12)
+        
+        # Seems to work in ~1128 hours
+        # assignments = []
+        # for x in tqdm(points):
+        #     assignments.append(self.get_centroid(x)) 
+
+        for input_composition, ind, distance in assignments:
             try:
-                new_entry = self.make_entry(point, distance)
+                new_entry = self.make_entry(input_composition, distance)
                 bisect.insort(self.centres[ind][1], new_entry)
             
                 if distance > self.centres[ind][2]:
@@ -125,16 +146,16 @@ class ElMTree():
 
             except Exception as e:
                 print(e)
-                print(point)
+                print(input_composition)
                 continue
 
-        print()
+        if self.verbose: print("ElMTree Constructed")
 
-    def make_entry(self, point, distance):
-        if isinstance(point, ElMD):
-            db_entries = self.elmtree_lookup[point.pretty_formula]
+    def make_entry(self, input_composition, distance):
+        if isinstance(input_composition, ElMD):
+            db_entries = self.elmtree_lookup[input_composition.pretty_formula]
         else:
-            db_entries = self.elmtree_lookup[ElMD(point).pretty_formula]
+            db_entries = self.elmtree_lookup[ElMD(input_composition).pretty_formula]
         
         experimental = False
         structure = False
@@ -148,13 +169,16 @@ class ElMTree():
                 if self.db_lookup[k]["structures"]:
                     structure = True
 
-        return Entry(point, distance, experimental, structure)
+        return Entry(input_composition, distance, experimental, structure)
 
-    def get_centroid(self, point):
-        distances = [self.metric(point, centre[0]) for centre in self.centres]
+    def get_centroid(self, input_composition):
+        # if self.verbose: print("Computing distances to each centroid")
+        input_composition = ElMD(input_composition)
+
+        distances = [self.metric(input_composition, centre[0]) for centre in self.centres]
         ind = np.argsort(distances)[0]
 
-        return (point, ind, distances[ind])
+        return (input_composition, ind, distances[ind])
 
     def metric(self, obj1, obj2, advanced_search=None):
         """Overload assigned metric function to allow more flexibility"""
@@ -164,17 +188,18 @@ class ElMTree():
             obj1 = pk.load(open(self.db_folder + str(obj1), "rb"))
             obj2 = pk.load(open(self.db_folder + str(obj2), "rb"))
 
-        if isinstance(obj1, str) or isinstance(obj2, str):
-            obj1, obj2 = ElMD(obj1), ElMD(obj2)
-            return self.assigned_metric(obj1, obj2)
+        if isinstance(obj1, str):
+            obj1 = ElMD(obj1)
 
-        else:
-            return self.assigned_metric(obj1, obj2)
+        if isinstance(obj2, str):
+            obj2 = ElMD(obj2)
+
+        return self.assigned_metric(obj1, obj2)
 
     def get_matches(self, query, advanced_search=None):
-        point, i, query_radius = query 
+        input_composition, i, query_radius = query 
         centre = self.centres[i]
-        distance = self.metric(point, centre[0])
+        distance = self.metric(input_composition, centre[0])
         
         ret = []
 
@@ -196,7 +221,7 @@ class ElMTree():
                        
                 # If the known distances could form a valid triangle
                 if abs(distance - leaf.distance) <= query_radius:
-                    leaf_distance = self.metric(leaf.entry, point)
+                    leaf_distance = self.metric(leaf.entry, input_composition)
 
                     if leaf_distance <= query_radius:
                         ret.append(self.make_entry(leaf.entry, leaf_distance))
@@ -225,7 +250,7 @@ class ElMTree():
 
         return [(x.entry, x.distance) for x in res]
 
-    def knn(self, query, k=5, advanced_search=None):
+    def knn(self, query, k=200, advanced_search=None):
         if not isinstance(query, ElMD):
             query = ElMD(query)
 
